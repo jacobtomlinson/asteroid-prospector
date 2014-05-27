b6eb6ad1da3a97b08547ebd5006a318a80c3863b
Kris Sum <kris@switchsystems.co.uk>
2014-05-27 16:12:53 +0100

    added low hardware mode in main game files

--- js/main.js
+++ js/main.js
@@ -125,13 +125,19 @@ require(
 
         world.add( asteroid );
 
-        //console.log((ship.state.pos.get(0) / 2) + " " + (ship.state.pos.get(1) / 2));
-        //console.log(document.body.style.backgroundPosition);
     }
 
     var init = function init( world, Physics ){
 
-    	world.options({timestep: 1000/30, maxIPF: 8}); // set the physics resolution to 30 fps
+    	var maxAsteroids = 50;
+
+    	if (lowHW==true) {
+    		world.options({timestep: 1000/20, maxIPF: 4}); // set the physics resolution to 30 fps
+    		maxAsteroids = 40;
+    	} else {
+    		world.options({timestep: 1000/30, maxIPF: 8}); // set the physics resolution to 30 fps
+    	}
+
 
         // create spaceship which will be controlled by the user
         var ship = Physics.body('player', {
@@ -147,7 +153,8 @@ require(
 
         // create asteroids
         var asteroids = [];
-        for ( var i = 0, l = 50; i < l; ++i ){
+
+        for ( var i = 0, l = maxAsteroids; i < l; ++i ){
             spawnAsteroid(Physics, world, ship, renderer);
         }
 
